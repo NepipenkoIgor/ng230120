6e120c40d1950f08d35d18cbd9e0bd8ca30a9e88
Ihor <igornepipenko@gmail.com>
2020-02-06 19:51:04 +0200

    Feature: async pipe [ref:no-ref]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatDrawer } from '@angular/material';
+import { IProducts, products$ } from './mock';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'course-root#test',
@@ -7,9 +9,34 @@ import { MatDrawer } from '@angular/material';
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'ng230120';
   public drawer !: MatDrawer;
+
+  public products$: Observable<IProducts[]> = products$;
+//  public products: IProducts[] = [];
+
+  public account = {
+    name: 'Ihor',
+    age: 33,
+  };
+
+  public ngOnInit(): void {
+    const sequence$ = interval(1000);
+    sequence$.subscribe((v) => {
+      console.log(v);
+    });
+
+    setTimeout(() => {
+      sequence$.subscribe((v) => {
+        console.log('Sub 2', v);
+      });
+    }, 5000);
+    // this.products$.subscribe((products) => {
+    //   console.log(products);
+    //   this.products = products;
+    // });
+  }
 
   public setSidenav(drawer: MatDrawer) {
     this.drawer = drawer;
